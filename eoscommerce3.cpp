5e41623bb947a6da796ebee04dfb28b625eaba21
Eldgar <walikanani@gmail.com>
2023-03-17 21:59:11 -0600

    added more functionality, reward stash and payouts functions

--- eoscommerce3.cpp
+++ eoscommerce3.cpp
@@ -1,8 +1,8 @@
 #include "eoscommerce3.hpp"
 
-const std::string_view waxString{"WAX"};
-const uint8_t waxdecimals = 8;
-const symbol waxsymbol(waxString, waxdecimals);
+const std::string_view wax_string{"WAX"};
+const uint8_t wax_decimals = 8;
+const symbol wax_symbol(wax_string, wax_decimals);
 
 const std::string_view ecom_string{"ECOM"};
 const uint8_t ecom_decimals = 4;
@@ -23,6 +23,84 @@ void eoscommerce3::erase() {
 }
 
 
+asset eoscommerce3::total_ecom() {
+    account_balance_table balances( _self, _self.value );
+
+    eosio::asset total_ecom_deposited(0, ecom_symbol);
+
+    for (const auto& row : balances) {
+        total_ecom_deposited += row.ecom_balance;
+    }
+
+    return total_ecom_deposited;
+}
+
+
+float eoscommerce3::ecomwax_ratio(name _self) {
+    rewards_balance_table rewards( _self, _self.value );
+
+    eosio::asset total_wax_deposited(0, wax_symbol);
+
+    for (const auto& row : rewards) {
+        total_wax_deposited += row.wax_balance;
+    }
+
+    // Call the total_ecom_deposited() function explicitly
+    eosio::asset total_ecom_deposited = total_ecom();
+
+    return (total_wax_deposited.amount / static_cast<float>(total_ecom_deposited.amount));
+}
+
+
+[[eosio::action]]
+void eoscommerce3::giverewards(){
+    require_auth(_self);
+    account_balance_table balances( _self, _self.value );
+
+    eosio::asset total_ecom_deposited = total_ecom();
+    eosio::asset total_wax(0, wax_symbol);
+    rewards_balance_table rewards( _self, _self.value );
+
+    for (const auto& row : rewards) {
+        total_wax += row.wax_balance;
+    }
+
+    // Iterate through the table and add wax balance to each entry
+    auto itr = balances.begin();
+    while (itr != balances.end()) {
+        balances.modify( itr, same_payer, [&](auto &row) {
+            uint64_t reward_wax_amount = (row.ecom_balance.amount * total_wax.amount) / total_ecom().amount;
+            row.wax_balance += eosio::asset(reward_wax_amount, wax_symbol);
+        });
+        itr++;
+    }
+		auto reward_itr = rewards.find( _self.value );
+				rewards.modify( reward_itr, same_payer, [&](auto &row) {
+				row.wax_balance += asset(0, wax_symbol);
+			});
+}
+
+
+[[eosio::action]]
+void eoscommerce3::addrewards(asset quantity) {
+  require_auth(_self); // Require authorization of the contract owner
+  	rewards_balance_table rewards(_self, _self.value);
+		auto itr = rewards.find( _self.value );
+	if( itr != rewards.end() ){
+				rewards.modify( itr, same_payer, [&](auto &row) {
+				row.wax_balance += quantity;
+			});
+		} else {
+			rewards.emplace( _self, [&](auto &row) {
+				row.user = _self.value;
+				row.wax_balance = quantity;
+			});
+
+		}
+
+}
+
+
 [[eosio::on_notify("eoscommtoken::transfer")]]
 void eoscommerce3::on_transfer(name from, name to, eosio::asset quantity, std::string memo) {
 		const eosio::name REAL_CONTRACT = "eoscommtoken"_n;
@@ -33,12 +111,12 @@ void eoscommerce3::on_transfer(name from, name to, eosio::asset quantity, std::s
 		check( quantity.amount >= 10000, "Quanity must be greater than 1 ECOM" );
 		check( REAL_CONTRACT == ORIGINAL_CONTRACT, "You tryna get over on us, bro?" );
 
-		if( from == get_self() || to != get_self() ){ return; }
+		if( from == _self || to != _self ){ return; }
 
 		check( quantity.symbol == ecom_symbol, "Symbol is not what we were expecting" );
 
 		//emplace info into table
-		account_balance_table balance( get_self(), get_self().value );
+		account_balance_table balance( _self, _self.value );
 		auto itr = balance.find( from.value );
 
 		if( itr != balance.end() ){
@@ -47,12 +125,12 @@ void eoscommerce3::on_transfer(name from, name to, eosio::asset quantity, std::s
 				row.ecom_balance += quantity;
 			});
 		} else {
-			balance.emplace( get_self(), [&](auto &row) {
+			balance.emplace( _self, [&](auto &row) {
 
 				row.user = from.value;
         		row.username = from;
 				row.ecom_balance = quantity;
-				row.wax_balance = asset(0, waxsymbol);
+				row.wax_balance = asset(0, wax_symbol);
 
 			});
 
@@ -60,7 +138,7 @@ void eoscommerce3::on_transfer(name from, name to, eosio::asset quantity, std::s
 
 		// action(permission_level{to, "active"_n}, 
 		// "eoscommtoken"_n, "transfer"_n, 
-		// std::make_tuple(eosio::name("eoscommerce3"), from, asset(100000, waxsymbol), std::string("We just reacted son"))).send();
+		// std::make_tuple(eosio::name("eoscommerce3"), from, asset(100000, wax_symbol), std::string("We just reacted son"))).send();
 
         
 }
--- eoscommerce3.hpp
+++ eoscommerce3.hpp
@@ -24,14 +24,34 @@ class [[eosio::contract("eoscommerce3")]] eoscommerce3 : public eosio::contract 
 		uint64_t primary_key() const { return user; }
 	  };
 
-    // Create a type alias for the account_balance table
+     // Create a type alias for the account_balance table
     using account_balance_table = eosio::multi_index<"balances"_n, balances>;
 
+
+    struct [[eosio::table]] rewards
+    {
+      uint64_t            user;          // Primary key: user account name
+      eosio::asset        wax_balance;   // WAX balance
+
+      // Getter for the primary key
+      uint64_t primary_key() const { return user; }
+	  };
+    using rewards_balance_table = eosio::multi_index<"rewards"_n, rewards>;
+   
     [[eosio::on_notify("eoscommtoken::transfer")]]
     void on_transfer(name from, name to, asset quantity, std::string memo);
 
     [[eosio::action]]
+    void addrewards(asset quantity);
+
+    [[eosio::action]]
+    void giverewards();
+
+    [[eosio::action]]
     void erase();
 
+    asset total_ecom();
+
+    float ecomwax_ratio(name _self);
 
 };
